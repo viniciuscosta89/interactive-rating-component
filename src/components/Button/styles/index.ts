8635a8330078deec886b84c4e9c6d2afe9c642b3
Vinicius Costa <vinicius.costa89@gmail.com>
2022-03-30 21:21:10 -0300

    fix(button): new StyledLink component for react-router Link

--- src/components/Button/styles/index.ts
+++ src/components/Button/styles/index.ts
@@ -1,36 +1,57 @@
-import styled, { css } from "styled-components";
+import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
 type ButtonTypes = {
-	variant: string,
-}
+  variant: string;
+};
 
 const variantStyles = (variant = 'primary') =>
-({
-  primary: css`
-    background-color: var(--primary);    
-    color: var(--white);
+  ({
+    primary: css`
+      background-color: var(--primary);
+      color: var(--white);
 
-    &:hover {
-      background-color: var(--white); 
-			color: var(--primary);      
-    }
-  `,  
-}[variant]);
+      &:hover {
+        background-color: var(--white);
+        color: var(--primary);
+      }
+    `,
+  }[variant]);
 
 export const Button = styled.button<ButtonTypes>`
-	position: relative;
-	border: none;
-	border-radius: 2em;
-	font-weight: 500;
-	letter-spacing: 0.25em;
-	padding: 1em 2em;
-	text-transform: uppercase;
-	width: 100%;
-	transition: var(--transition);
+  position: relative;
+  border: none;
+  border-radius: 2em;
+  font-weight: 500;
+  letter-spacing: 0.25em;
+  padding: 1em 2em;
+  text-transform: uppercase;
+  width: 100%;
+  transition: var(--transition);
 
-	&:hover {
-		cursor: pointer;
-	}
+  &:hover {
+    cursor: pointer;
+  }
 
-	${({ variant }) => variantStyles(variant)};
-`
+  ${({ variant }) => variantStyles(variant)};
+`;
+
+export const StyledLink = styled(Link)<ButtonTypes>`
+  position: relative;
+  border: none;
+  border-radius: 2em;
+  display: block;
+  font-weight: 500;
+  letter-spacing: 0.25em;
+  padding: 1em 2em;
+  text-align: center;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition: var(--transition);
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  ${({ variant }) => variantStyles(variant)};
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,52 @@
-import Button from '../components/Button';
-import {Card, Header, Main, Ratings } from '../components/Card';
+import { Card, Header, Main, Ratings } from '../components/Card';
 import IconStar from '../assets/icon-star.svg?component';
-import { Link } from 'react-router-dom';
 import { useRating } from '../hooks/useRating';
 import AnimatedPage from '../components/AnimatedPage';
+import { StyledLink } from '../components/Button/styles';
 
 const Home = () => {
-	const { rating, changeRating } = useRating();
+  const { rating, changeRating } = useRating();
 
-	const ratingNumbers = [
-		{ id: 1, value: 1 }, 
-		{ id: 2, value: 2 },
-		{ id: 3, value: 3 },
-		{ id: 4, value: 4 },
-		{ id: 5, value: 5 }
-	]
+  const ratingNumbers = [
+    { id: 1, value: 1 },
+    { id: 2, value: 2 },
+    { id: 3, value: 3 },
+    { id: 4, value: 4 },
+    { id: 5, value: 5 },
+  ];
 
-	return (		
-		<AnimatedPage>
-			<Card>
-				<Header>
-					<div className="card__icon-wrapper">
-						<IconStar className="card__icon" />
-					</div>
-				</Header>
-				<Main>
-					<h1>How did we do?</h1>
-					<p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+  return (
+    <AnimatedPage>
+      <Card>
+        <Header>
+          <div className='card__icon-wrapper'>
+            <IconStar className='card__icon' />
+          </div>
+        </Header>
+        <Main>
+          <h1>How did we do?</h1>
+          <p>
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
+          </p>
 
-					<Ratings>
-						{ratingNumbers.map(item => 
-							<button 
-								key={item.id}
-								onClick={() => changeRating(item.id)}
-								className={ rating === item.id ? 'active': ''}>
-									{item.value}
-							</button>
-						)}
-					</Ratings>			
+          <Ratings>
+            {ratingNumbers.map(item => (
+              <button
+                key={item.id}
+                onClick={() => changeRating(item.id)}
+                className={rating === item.id ? 'active' : ''}
+              >
+                {item.value}
+              </button>
+            ))}
+          </Ratings>
 
-					<Link to="/thank-you">
-						<Button variant="primary">Submit</Button>
-					</Link>
-				</Main>				
-			</Card>
-		</AnimatedPage>
-	)
-}
+          <StyledLink to='/thank-you'>Submit</StyledLink>
+        </Main>
+      </Card>
+    </AnimatedPage>
+  );
+};
 
 export default Home;
